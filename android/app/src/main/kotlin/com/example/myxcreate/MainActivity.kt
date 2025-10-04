6d7d84b65faa6cb6b2f9a28c7bc4ce6edbbcf006
lazuardi112 <110966555+lazuardi112@users.noreply.github.com>
2025-10-05 03:22:34 +0700

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/myxcreate/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/myxcreate/MainActivity.kt
@@ -1,5 +1,46 @@
 package com.example.myxcreate
 
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import androidx.annotation.NonNull
+import androidx.core.content.ContextCompat
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.example.myxcreate/xc_service"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "startForeground" -> {
+                    startForegroundServiceNative()
+                    result.success(true)
+                }
+                "stopForeground" -> {
+                    stopForegroundServiceNative()
+                    result.success(true)
+                }
+                else -> result.notImplemented()
+            }
+        }
+    }
+
+    private fun startForegroundServiceNative() {
+        val intent = Intent(this, ForegroundStarterService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            ContextCompat.startForegroundService(this, intent)
+        } else {
+            startService(intent)
+        }
+    }
+
+    private fun stopForegroundServiceNative() {
+        val intent = Intent(this, ForegroundStarterService::class.java)
+        stopService(intent)
+    }
+}
