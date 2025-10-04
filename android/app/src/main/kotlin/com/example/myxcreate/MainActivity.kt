74c0aef35f4c08adbe482a1c73fc4098754b2c6a
lazuardi112 <110966555+lazuardi112@users.noreply.github.com>
2025-10-05 03:48:59 +0700

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/myxcreate/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/myxcreate/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.myxcreate
 
 import android.content.Intent
 import android.os.Build
-import android.os.Bundle
-import androidx.annotation.NonNull
 import androidx.core.content.ContextCompat
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
@@ -12,9 +10,8 @@ import io.flutter.plugin.common.MethodChannel
 class MainActivity: FlutterActivity() {
     private val CHANNEL = "com.example.myxcreate/xc_service"
 
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
             when (call.method) {
                 "startForeground" -> {
