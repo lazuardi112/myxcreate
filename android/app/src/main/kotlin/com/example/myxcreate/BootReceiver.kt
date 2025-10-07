476fef9c277fe0baf43bab6a6ecf02451767d991
lazuardi112 <110966555+lazuardi112@users.noreply.github.com>
2025-10-07 11:30:19 +0700

    Update BootReceiver.kt

--- android/app/src/main/kotlin/com/example/myxcreate/BootReceiver.kt
+++ android/app/src/main/kotlin/com/example/myxcreate/BootReceiver.kt
@@ -1,24 +1,19 @@
+// android/app/src/main/kotlin/com/example/myxcreate/BootReceiver.kt
 package com.example.myxcreate
 
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import androidx.core.content.ContextCompat
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent?) {
         if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
-            val prefs = context.getSharedPreferences("FlutterSharedPreferences", Context.MODE_PRIVATE)
-            val enabled = prefs.getBoolean("xc_listener_enabled", false)
+            val sp = context.getSharedPreferences("myxcreate_prefs", Context.MODE_PRIVATE)
+            val enabled = sp.getBoolean("enabled", false)
             if (enabled) {
-                try {
-                    val i = Intent(context, ForegroundStarterService::class.java)
-                    ContextCompat.startForegroundService(context, i)
-                    Log.d("BootReceiver", "Started ForegroundStarterService on boot")
-                } catch (e: Exception) {
-                    Log.w("BootReceiver", "failed start on boot: $e")
-                }
+                val i = Intent(context, ForegroundStarterService::class.java)
+                ContextCompat.startForegroundService(context, i)
             }
         }
     }
